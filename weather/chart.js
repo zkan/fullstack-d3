3c6eba44cc6dba443e89f9557002c99af261cfdb
zkan <zkan.cs@gmail.com>
2021-04-17 22:17:45 +0700

    Extract some value and parse date using D3 method :bear:

--- weather/chart.js
+++ weather/chart.js
@@ -3,6 +3,14 @@ import * as d3 from "d3"
 async function drawLineChart() {
   const data = await d3.json('./data/my_weather_data.json')
   console.log(data)
+
+  const yAccessor = d => d['temperatureMax']
+  console.log(yAccessor(data[0]))
+
+  const parseDate = d3.timeParse('%Y-%m-%d')
+
+  const xAccessor = d => parseDate(d['date'])
+  console.log(xAccessor(data[0]))
 }
 
 drawLineChart()
