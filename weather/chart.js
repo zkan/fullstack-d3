8f6429f5b650bbf9a75aaa3b7ce3cd0b1a64d03c
zkan <zkan.cs@gmail.com>
2021-04-24 21:13:46 +0700

    Draw bars :bear:

--- weather/chart.js
+++ weather/chart.js
@@ -135,9 +135,9 @@ async function drawScatter() {
 
   const wrapper = d3.select('#wrapper-scatter')
     .append('svg')
-    .attr('width', dimensions.width)
-    .attr('height', dimensions.height)
-    // .style('border', '1px solid')
+      .attr('width', dimensions.width)
+      .attr('height', dimensions.height)
+      // .style('border', '1px solid')
 
   const bounds = wrapper.append('g')
     .style('transform', `translate(${
@@ -218,5 +218,82 @@ async function drawScatter() {
       .style('text-anchor', 'middle')
 }
 
+async function drawBars() {
+  const data = await d3.json('./data/my_weather_data.json')
+
+  const xAccessor = d => d.humidity
+  console.log(xAccessor(data[0]))
+  const yAccessor = d => d.length
+
+  const width = 600
+  let dimensions = {
+    width,
+    height: width * 0.6,
+    margin: {
+      top: 30,
+      right: 10,
+      bottom: 50,
+      left: 50,
+    }
+  }
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.left
+    - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom
+
+  const wrapper = d3.select('#wrapper-bars')
+    .append('svg')
+      .attr('width', dimensions.width)
+      .attr('height', dimensions.height)
+
+  const bounds = wrapper.append('g')
+      .style('transform', `translate(${
+        dimensions.margin.left
+      }px, ${
+        dimensions.margin.top
+      }px)`)
+
+  const xScale = d3.scaleLinear()
+      .domain(d3.extent(data, xAccessor))
+      .range([0, dimensions.boundedWidth])
+      .nice()
+  console.log(xScale.domain())
+
+  const binGenerator = d3.bin()
+      .domain(xScale.domain())
+      .value(xAccessor)
+      .thresholds(12) // space between bars -- if we want 13 bars, then we get 12 spaces
+
+  const bins = binGenerator(data)
+  console.log(bins)
+
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(bins, yAccessor)])
+    .range([dimensions.boundedHeight, 0])
+    .nice()
+  console.log(yScale.domain())
+
+  const binsGroup = bounds.append('g')
+
+  const binGroups = binsGroup.selectAll('g')
+      .data(bins)
+      .join('g')
+
+  const barPadding = 1
+
+  const barRects = binGroups.append('rect')
+      .attr('x', d => xScale(d.x0) + barPadding)
+      .attr('y', d => yScale(yAccessor(d)))
+      .attr('width', d => d3.max([
+        0,
+        xScale(d.x1) - xScale(d.x0) - barPadding
+      ]))
+      .attr('height', d => dimensions.boundedHeight - yScale(yAccessor(d)))
+      .attr('fill', 'cornflowerblue')
+}
+
 drawLineChart()
 drawScatter()
+drawBars()
