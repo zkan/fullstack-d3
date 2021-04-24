0b297c364419e85b32d28eb351e1946d00961bb3
zkan <zkan.cs@gmail.com>
2021-04-24 19:27:04 +0700

    Draw scatter plot :bear:

--- weather/chart.js
+++ weather/chart.js
@@ -100,4 +100,84 @@ async function drawLineChart() {
       }px)`)
 }
 
+async function drawScatter() {
+  const data = await d3.json('./data/my_weather_data.json')
+
+  const xAccessor = d => d.dewPoint
+  const yAccessor = d => d.humidity
+
+  console.log(xAccessor(data[0]))
+  console.log(yAccessor(data[0]))
+
+  const width = d3.min([
+    window.innerWidth * 0.9,
+    window.innerHeight * 0.9,
+  ])
+  console.log(width)
+  const dimensions = {
+    width,
+    height: width,
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 15,
+      left: 15,
+    }
+  }
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margin.right
+    - dimensions.margin.left
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margin.top
+    - dimensions.margin.bottom
+
+  const wrapper = d3.select('#wrapper-scatter')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
+    .style('border', '1px solid')
+
+  const bounds = wrapper.append('g')
+    .style('transform', `translate(${
+      dimensions.margin.left
+    }px, ${
+      dimensions.margin.top
+    }px)`)
+
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(data, xAccessor))
+    .range([0, dimensions.boundedWidth])
+    .nice()
+  console.log(xScale.domain())
+
+  const yScale = d3.scaleLinear()
+    .domain(d3.extent(data, yAccessor))
+    .range([dimensions.boundedHeight, 0])
+    .nice()
+  console.log(yScale.domain())
+
+  // data.forEach(d => {
+  //   bounds.append('circle')
+  //     .attr('cx', xScale(xAccessor(d)))
+  //     .attr('cy', yScale(yAccessor(d)))
+  //     .attr('r', 5)
+  // })
+
+  // const dots = bounds.selectAll('circle')
+  //     .data(data).enter().append('circle')
+  //       .attr('cx', d => xScale(xAccessor(d)))
+  //       .attr('cy', d => yScale(yAccessor(d)))
+  //       .attr('r', 5)
+  //       .attr('fill', 'cornflowerblue')
+
+  const dots = bounds.selectAll('circle')
+      .data(data).join('circle')
+        .attr('cx', d => xScale(xAccessor(d)))
+        .attr('cy', d => yScale(yAccessor(d)))
+        .attr('r', 5)
+        .attr('fill', 'cornflowerblue')
+  console.log(dots)
+}
+
 drawLineChart()
+drawScatter()
