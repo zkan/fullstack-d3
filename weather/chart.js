5ac63554b270bb3c631e9de43f5c64ad75a80f57
zkan <zkan.cs@gmail.com>
2021-04-24 21:54:05 +0700

    Draw more then one histograms :bear:

--- weather/chart.js
+++ weather/chart.js
@@ -221,124 +221,137 @@ async function drawScatter() {
 async function drawBars() {
   const data = await d3.json('./data/my_weather_data.json')
 
-  const xAccessor = d => d.humidity
-  console.log(xAccessor(data[0]))
-  const yAccessor = d => d.length
+  const drawHistogram = metric => {
+    const xAccessor = d => d[metric]
+    console.log(xAccessor(data[0]))
+    const yAccessor = d => d.length
 
-  const width = 600
-  let dimensions = {
-    width,
-    height: width * 0.6,
-    margin: {
-      top: 30,
-      right: 10,
-      bottom: 50,
-      left: 50,
+    const width = 600
+    let dimensions = {
+      width,
+      height: width * 0.6,
+      margin: {
+        top: 30,
+        right: 10,
+        bottom: 50,
+        left: 50,
+      }
     }
-  }
-  dimensions.boundedWidth = dimensions.width
-    - dimensions.margin.left
-    - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height
-    - dimensions.margin.top
-    - dimensions.margin.bottom
+    dimensions.boundedWidth = dimensions.width
+      - dimensions.margin.left
+      - dimensions.margin.right
+    dimensions.boundedHeight = dimensions.height
+      - dimensions.margin.top
+      - dimensions.margin.bottom
 
-  const wrapper = d3.select('#wrapper-bars')
-    .append('svg')
-      .attr('width', dimensions.width)
-      .attr('height', dimensions.height)
+    const wrapper = d3.select('#wrapper-bars')
+      .append('svg')
+        .attr('width', dimensions.width)
+        .attr('height', dimensions.height)
 
-  const bounds = wrapper.append('g')
-      .style('transform', `translate(${
-        dimensions.margin.left
-      }px, ${
-        dimensions.margin.top
-      }px)`)
+    const bounds = wrapper.append('g')
+        .style('transform', `translate(${
+          dimensions.margin.left
+        }px, ${
+          dimensions.margin.top
+        }px)`)
 
-  const xScale = d3.scaleLinear()
-      .domain(d3.extent(data, xAccessor))
-      .range([0, dimensions.boundedWidth])
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, xAccessor))
+        .range([0, dimensions.boundedWidth])
+        .nice()
+    console.log(xScale.domain())
+
+    const binGenerator = d3.bin()
+        .domain(xScale.domain())
+        .value(xAccessor)
+        .thresholds(12) // space between bars -- if we want 13 bars, then we get 12 spaces
+
+    const bins = binGenerator(data)
+    console.log(bins)
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(bins, yAccessor)])
+      .range([dimensions.boundedHeight, 0])
       .nice()
-  console.log(xScale.domain())
+    console.log(yScale.domain())
 
-  const binGenerator = d3.bin()
-      .domain(xScale.domain())
-      .value(xAccessor)
-      .thresholds(12) // space between bars -- if we want 13 bars, then we get 12 spaces
+    const binsGroup = bounds.append('g')
 
-  const bins = binGenerator(data)
-  console.log(bins)
+    const binGroups = binsGroup.selectAll('g')
+        .data(bins)
+        .join('g')
 
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(bins, yAccessor)])
-    .range([dimensions.boundedHeight, 0])
-    .nice()
-  console.log(yScale.domain())
+    const barPadding = 1
 
-  const binsGroup = bounds.append('g')
+    const barRects = binGroups.append('rect')
+        .attr('x', d => xScale(d.x0) + barPadding / 2)
+        .attr('y', d => yScale(yAccessor(d)))
+        .attr('width', d => d3.max([
+          0,
+          xScale(d.x1) - xScale(d.x0) - barPadding
+        ]))
+        .attr('height', d => dimensions.boundedHeight - yScale(yAccessor(d)))
+        .attr('fill', 'cornflowerblue')
 
-  const binGroups = binsGroup.selectAll('g')
-      .data(bins)
-      .join('g')
+    const barText = binGroups.filter(yAccessor)
+        .append('text')
+        .attr('x', d => xScale(d.x0) + (
+          xScale(d.x1) - xScale(d.x0)
+        ) / 2)
+        .attr('y', d => yScale(yAccessor(d)) - 5)
+        .text(yAccessor)
+        .style('text-anchor', 'middle')
+        .style('fill', '#666')
+        .style('font-size', '12px')
+        .style('font-family', 'sans-serif')
 
-  const barPadding = 1
+    const mean = d3.mean(data, xAccessor)
+    console.log(mean)
 
-  const barRects = binGroups.append('rect')
-      .attr('x', d => xScale(d.x0) + barPadding / 2)
-      .attr('y', d => yScale(yAccessor(d)))
-      .attr('width', d => d3.max([
-        0,
-        xScale(d.x1) - xScale(d.x0) - barPadding
-      ]))
-      .attr('height', d => dimensions.boundedHeight - yScale(yAccessor(d)))
-      .attr('fill', 'cornflowerblue')
+    const meanLine = bounds.append('line')
+        .attr('x1', xScale(mean))
+        .attr('x2', xScale(mean))
+        .attr('y1', -15)
+        .attr('y2', dimensions.boundedHeight)
+        .attr('stroke', 'maroon')
+        .style('stroke-dasharray', '2px 4px')
 
-  const barText = binGroups.filter(yAccessor)
-      .append('text')
-      .attr('x', d => xScale(d.x0) + (
-        xScale(d.x1) - xScale(d.x0)
-      ) / 2)
-      .attr('y', d => yScale(yAccessor(d)) - 5)
-      .text(yAccessor)
-      .style('text-anchor', 'middle')
-      .style('fill', '#666')
-      .style('font-size', '12px')
-      .style('font-family', 'sans-serif')
+    const meanLabel = bounds.append('text')
+        .attr('x', xScale(mean))
+        .attr('y', -20)
+        .text('mean')
+        .style('text-anchor', 'middle')
+        .style('font-family', 'sans-serif')
+        .style('fill', 'maroon')
+        .style('font-size', '12px')
 
-  const mean = d3.mean(data, xAccessor)
-  console.log(mean)
+    const xAxisGenerator = d3.axisBottom()
+        .scale(xScale)
+    const xAxis = bounds.append('g')
+        .call(xAxisGenerator)
+        .style('transform', `translateY(${
+          dimensions.boundedHeight
+        }px)`)
 
-  const meanLine = bounds.append('line')
-      .attr('x1', xScale(mean))
-      .attr('x2', xScale(mean))
-      .attr('y1', -15)
-      .attr('y2', dimensions.boundedHeight)
-      .attr('stroke', 'maroon')
-      .style('stroke-dasharray', '2px 4px')
+    const xAxisLabel = xAxis.append('text')
+        .attr('x', dimensions.boundedWidth / 2)
+        .attr('y', dimensions.margin.bottom - 10)
+        .attr('fill', 'black')
+        .style('font-size', '1.4em')
+        .text(metric)
+  }
 
-  const meanLabel = bounds.append('text')
-      .attr('x', xScale(mean))
-      .attr('y', -20)
-      .text('mean')
-      .style('text-anchor', 'middle')
-      .style('font-family', 'sans-serif')
-      .style('fill', 'maroon')
-      .style('font-size', '12px')
-
-  const xAxisGenerator = d3.axisBottom()
-      .scale(xScale)
-  const xAxis = bounds.append('g')
-      .call(xAxisGenerator)
-      .style('transform', `translateY(${
-        dimensions.boundedHeight
-      }px)`)
-
-  const xAxisLabel = xAxis.append('text')
-      .attr('x', dimensions.boundedWidth / 2)
-      .attr('y', dimensions.margin.bottom - 10)
-      .attr('fill', 'black')
-      .style('font-size', '1.4em')
-      .text('Humidity')
+  const metrics = [
+    'humidity',
+    'windSpeed',
+    'dewPoint',
+    'uvIndex',
+    'visibility',
+    'temperatureMax',
+    'cloudCover',
+  ]
+  metrics.forEach(drawHistogram)
 }
 
 drawLineChart()
