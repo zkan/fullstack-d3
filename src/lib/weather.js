6e9f9dff7805c0ccaa32e8f26dd8ad05ae35e799
zkan <kan@odds.team>
2025-03-28 19:15:16 +0700

    Set y and x scale + draw freezing temperature placement

--- src/lib/weather.js
+++ src/lib/weather.js
@@ -1,70 +1,73 @@
 import * as d3 from "d3"
 
 async function drawLineChart() {
-  const data = await d3.json('./my_weather_data.json')
+  const data = await d3.json("./my_weather_data.json")
   console.log(data)
 
   // Accessor functions to convert a single data point into metric value
-  const yAccessor = d => d['temperatureMax']
+  const yAccessor = d => d["temperatureMax"]
   console.log(yAccessor(data[0]))
 
-  const parseDate = d3.timeParse('%Y-%m-%d')
+  const parseDate = d3.timeParse("%Y-%m-%d")
 
-  const xAccessor = d => parseDate(d['date'])
+  const xAccessor = d => parseDate(d["date"])
   console.log(xAccessor(data[0]))
 
-//   let dimensions = {
-//     width: window.innerWidth * 0.9,
-//     height: 400,
-//     margins: {
-//       top: 15,
-//       right: 15,
-//       bottom: 40,
-//       left: 60,
-//     }
-//   }
-//   dimensions.boundedWidth = dimensions.width
-//     - dimensions.margins.left
-//     - dimensions.margins.right
-//   dimensions.boundedHeight = dimensions.height
-//     - dimensions.margins.top
-//     - dimensions.margins.bottom
-//   console.log(dimensions)
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margins: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    }
+  }
+  dimensions.boundedWidth = dimensions.width
+    - dimensions.margins.left
+    - dimensions.margins.right
+  dimensions.boundedHeight = dimensions.height
+    - dimensions.margins.top
+    - dimensions.margins.bottom
+  console.log(dimensions)
 
-//   const wrapper = d3.select('#wrapper')
-//     .append('svg')
-//       .attr('width', dimensions.width)
-//       .attr('height', dimensions.height)
-//   console.log(wrapper)
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+  console.log(wrapper)
 
-//   const bounds = wrapper.append('g')
-//       .style('transform', `translate(${
-//         dimensions.margins.left
-//       }px, ${
-//         dimensions.margins.top
-//       }px)`)
-//   console.log(bounds)
+  const bounds = wrapper.append("g")
+    .style("transform", `translate(${
+      dimensions.margins.left
+    }px, ${
+      dimensions.margins.top
+    }px)`)
+  console.log(bounds)
 
-//   const yScale = d3.scaleLinear()
-//       .domain(d3.extent(data, yAccessor)) // input space: minimum and maximum value
-//       .range([dimensions.boundedHeight, 0]) // output space
+  const yScale = d3.scaleLinear()
+      .domain(d3.extent(data, yAccessor)) // input space: minimum and maximum value
+      .range([dimensions.boundedHeight, 0]) // output space
 
-//   // This means that if we have a 0, we will plot a point yScale(0) from the top
-//   console.log(yScale(0))
-//   console.log(yScale(50))
-//   console.log(yScale(100))
+  console.log(d3.extent(data, yAccessor))
 
-//   const freezingTemperaturePlacement = yScale(32)
-//   const freezingTemperatures = bounds.append('rect')
-//       .attr('x', 0)
-//       .attr('width', dimensions.boundedWidth)
-//       .attr('y', freezingTemperaturePlacement)
-//       .attr('height', dimensions.boundedHeight - freezingTemperaturePlacement)
-//       .attr('fill', '#e0f3f3')
+  // This means that if we have a 0, we will plot a point yScale(0) from the top
+  console.log(yScale(0))
+  console.log(yScale(13.02))
+  console.log(yScale(50))
+  console.log(yScale(100))
 
-//   const xScale = d3.scaleTime()
-//       .domain(d3.extent(data, xAccessor))
-//       .range([0, dimensions.boundedWidth])
+  const freezingTemperaturePlacement = yScale(32)
+  const freezingTemperatures = bounds.append("rect")
+      .attr("x", 0)
+      .attr("width", dimensions.boundedWidth)
+      .attr("y", freezingTemperaturePlacement)
+      .attr("height", dimensions.boundedHeight - freezingTemperaturePlacement)
+      .attr("fill", "#e0f3f3")
+
+  const xScale = d3.scaleTime()
+      .domain(d3.extent(data, xAccessor))
+      .range([0, dimensions.boundedWidth])
 
 //   // Draw data
 
